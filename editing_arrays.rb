31db58add57dd098aa1ab7988d584e0c3ed5ebf9
Julianne Kuettner <julianne.kuettner@thenobleacademy.org>
2018-08-08 16:46:20 +0000

    automatically backed up by learn

--- editing_arrays.rb
+++ editing_arrays.rb
@@ -12,4 +12,6 @@ end
 
 def add_colors
   # Write a solution that adds "green", then "blue" to the rainbow_colors array, then returns the array
+  rainbow_colors = ['green','blue']
+  return rainbow_colors
 end
